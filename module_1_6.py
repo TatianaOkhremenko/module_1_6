f396ec0e9383a824ba06dbbeaee3c375e7ac25a4
TatianaOkhremenko <tafinka@mail.ru>
2024-08-13 16:08:46 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,20 @@
+my_dick = {'a': 12,'b': 13,'c': 14}
+print(my_dick)
+print(my_dick['a'])
+print(my_dick.get('d'))
+my_dick.update({'f':16,'g':17})
+print(my_dick)
+a = my_dick.pop('a')
+print(my_dick)
+print(a)
+print(my_dick)
+
+my_set = {1,2,3,3,5,5,'Tatiana','Anna','Anna'}
+print(my_set)
+print(my_set.add(6))
+print(my_set.add(8))
+print(my_set)
+print(my_set.discard(2))
+print(my_set)
+average = sum(grades[0])/len(grades[0]),sum(grades[1])/len(grades[1]),sum(grades[2])/len(grades[2]), sum(grades[3])/len(grades[3]),sum(grades[4])/len(grades[4])
+print(average)
